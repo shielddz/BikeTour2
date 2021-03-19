200bc4f10ba027fd49d04e54a37ca3ff9d8f3179
Nassima1227 <ramdani.nassima16@gmail.com>
2021-03-19 10:24:44 +0100

    API-Post-Accelerometers

--- API/routes/accelerometers.js
+++ API/routes/accelerometers.js
@@ -7,7 +7,31 @@ const Accelerometer = require('../models/Accelerometer');
 //Import the user model for the helper function
 const User = require('../models/User');
 
+// Add a collected accelerometer data 
+router.post('/', async (req, res) => {
+    // check for user existance 
+    let userExists = await checkKeyAdd(req.body.key);
 
+    if(userExists){
+        // Create a new accelerometer data
+        const accelerometer = new Accelerometer({
+            key: req.body.key,
+            X: req.body.X,
+            Y: req.body.Y,
+            Z: req.body.Z,
+            time: req.body.time
+        });
+        try {
+            // Check if adding it is possible or not
+            const savedAccelerometer = await accelerometer.save();
+            res.json(savedAccelerometer);
+        } catch (err) {
+            res.json({message: err});
+        }
+    }else{
+        res.json({message: "No matching key found"});
+    }
+})
 
 // export the route
 module.exports = router;
